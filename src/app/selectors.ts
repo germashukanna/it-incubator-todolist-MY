a3ba2294e3a9f9d5ba3b519a7499bad05e849ee2
germashukanna <germashuk@gmail.com>
2023-03-03 18:45:37 +0300

    Updates

--- src/app/selectors.ts
+++
@@ -1,5 +0,0 @@
-import {AppRootStateType} from "../utils/types";
-
-
-export const selectStatus = (state: AppRootStateType) => state.appReducer.status
-export const selectInitialized = (state: AppRootStateType) => state.appReducer.isInitialized
--- src/features/AplicationCommonActions.tsx
+++
@@ -1,7 +0,0 @@
-import {createAction} from "@reduxjs/toolkit";
-import {RequestStatusType} from "./App/app-reducer";
-
-
-export const setAppStatusAC = createAction<{ status: RequestStatusType }>('app/setAppStatusAC')
-
-export const errorAppStatusAC = createAction<{ error: string | null }>('app/errorAppStatusAC')
